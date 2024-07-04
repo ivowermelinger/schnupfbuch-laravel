b5e32c40c7e43e0266d9ac4961c78131d05ff208
Ivo Wermelinger <ivo_wermelinger@hotmail.com>
2024-07-04 14:52:28 +0200

    Add modal component setup

--- resources/pages/auth/Login.svelte
+++ resources/pages/auth/Login.svelte
@@ -1,4 +1,5 @@
 <script>
+	import Modal from '../components/Modal.svelte';
 	import { showLogin, flashMessage } from '../stores';
 	import Close from '../svg/Close.svelte';
 
@@ -19,39 +20,30 @@
 	};
 </script>
 
-<div class="login__wrapper" class:login__wrapper--show={show}>
-	<button class="login__back" on:click|preventDefault={hideForm}>
-		<span class="visually-hidden">Login schliessen</span>
-	</button>
-	<div class="login__inner">
-		<button class="login__close" on:click|preventDefault={hideForm}>
-			<span class="visually-hidden">Login schliessen</span>
-			<Close css="login__icon" />
-		</button>
-		<div class="container">
-			<div class="row">
-				<div class="col-12">
-					<form class="form">
-						<div class="form__group">
-							<label class="form__label form__label--dark" for="email">E-Mail</label>
-							<input class="form__input form__input--dark" type="email" id="email" required autocomplete="email" />
-						</div>
-						<div class="form__group">
-							<label class="form__label form__label--dark" for="password">Passwort</label>
-							<input class="form__input form__input--dark" type="passsword" name="password" id="password" required autocomplete="current-password" />
-						</div>
-					</form>
-
-					{#if flash}
-						<p class="login__message">{flash}</p>
-					{/if}
-
-					<div class="login__links">
-						<a href="/register" class="login__link">Registrieren</a>
-						<a href="/password/reset" class="login__link">Passwort vergessen?</a>
+<Modal {show} closeModal={hideForm} name="Login Formular">
+	<div class="container">
+		<div class="row">
+			<div class="col-12">
+				<form class="form">
+					<div class="form__group">
+						<label class="form__label form__label--dark" for="email">E-Mail</label>
+						<input class="form__input form__input--dark" type="email" id="email" required autocomplete="email" />
 					</div>
+					<div class="form__group">
+						<label class="form__label form__label--dark" for="password">Passwort</label>
+						<input class="form__input form__input--dark" type="passsword" name="password" id="password" required autocomplete="current-password" />
+					</div>
+				</form>
+
+				{#if flash}
+					<p class="login__message">{flash}</p>
+				{/if}
+
+				<div class="login__links">
+					<a href="/register" class="login__link">Registrieren</a>
+					<a href="/password/reset" class="login__link">Passwort vergessen?</a>
 				</div>
 			</div>
 		</div>
 	</div>
-</div>
+</Modal>
---
+++ resources/pages/components/Modal.svelte
@@ -0,0 +1,22 @@
+<script>
+	import Close from '../svg/Close.svelte';
+
+	export let show = false;
+	export let closeModal = () => {
+		show = false;
+	};
+	export let name = 'Modal';
+</script>
+
+<div class="modal__wrapper" class:modal__wrapper--show={show}>
+	<button class="modal__back" on:click|preventDefault={closeModal}>
+		<span class="visually-hidden">{`${name} schliessen`}</span>
+	</button>
+	<div class="modal__inner">
+		<button class="modal__close" on:click|preventDefault={closeModal}>
+			<span class="visually-hidden">{`${name} schliessen`}</span>
+			<Close css="modal__icon" />
+		</button>
+		<slot />
+	</div>
+</div>
