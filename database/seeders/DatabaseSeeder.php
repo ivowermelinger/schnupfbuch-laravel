82209927cdea1d94d6306edce97ea8f82bdcfcff
Ivo Wermelinger <ivo_wermelinger@hotmail.com>
2024-07-28 15:31:22 +0200

    New seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -6,8 +6,7 @@ use App\Models\User;
 use App\Models\Line;
 use App\Enums\Roles;
 use Illuminate\Support\Str;
-
-// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Support\Facades\Log;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -17,16 +16,36 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-         User::factory(5)->create();
-         Line::factory(5)->create();
+        User::factory(5)->create();
+        Line::factory(5)->create();
 
-         // Create a admin user
+        // Get environment variables for admin user
+        $adminFirstName = env('ADMIN_FIRST_NAME');
+        $adminLastName = env('ADMIN_LAST_NAME');
+        $adminNickname = env('ADMIN_NICKNAME');
+        $adminEmail = env('ADMIN_EMAIL');
+        $adminPassword = env('ADMIN_PASSWORD');
+
+        // Log the admin user environment variables
+        Log::info('Admin User Environment Variables', [
+            'first_name' => $adminFirstName,
+            'last_name' => $adminLastName,
+            'nickname' => $adminNickname,
+            'email' => $adminEmail,
+        ]);
+
+        // Check if any of the required environment variables are missing
+        if (empty($adminFirstName) || empty($adminLastName) || empty($adminNickname) || empty($adminEmail) || empty($adminPassword)) {
+            throw new \Exception('Missing environment variables for admin user.');
+        }
+
+        // Create an admin user
         User::create([
-            'first_name' => env('ADMIN_FIRST_NAME'),
-            'last_name' => env('ADMIN_LAST_NAME'),
-            'nickname' => env('ADMIN_NICKNAME'),
-            'email' => env('ADMIN_EMAIL'),
-            'password' => bcrypt(env('ADMIN_PASSWORD')),
+            'first_name' => $adminFirstName,
+            'last_name' => $adminLastName,
+            'nickname' => $adminNickname,
+            'email' => $adminEmail,
+            'password' => bcrypt($adminPassword),
             'email_verified_at' => now(),
             'role' => Roles::ADMIN,
             'remember_token' => Str::random(12),
