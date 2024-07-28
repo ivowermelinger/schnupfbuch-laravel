c6972f562b33523009f3eabc6dfa72233013cdf9
Ivo Wermelinger <ivo_wermelinger@hotmail.com>
2024-07-28 15:41:09 +0200

    inertia debug

--- resources/assets/ts/app.ts
+++ resources/assets/ts/app.ts
@@ -1,11 +1,14 @@
 import { createInertiaApp } from '@inertiajs/svelte';
 
 createInertiaApp({
-	resolve: (name) => {
-		const pages = import.meta.glob('../../Pages/**/*.svelte', { eager: true });
-		return pages[`../../Pages/${name}.svelte`];
-	},
-	setup({ el, App, props }) {
-		new App({ target: el, props });
-	},
+    resolve: (name) => {
+        const pages = import.meta.glob('../../Pages/**/*.svelte', {
+            eager: true,
+        });
+        console.log(pages, name);
+        return pages[`../../Pages/${name}.svelte`];
+    },
+    setup({ el, App, props }) {
+        new App({ target: el, props });
+    },
 });
