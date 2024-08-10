5d389ee7059c020d1acc2e35233bb989cba43215
Ivo Wermelinger <ivo_wermelinger@hotmail.com>
2024-08-10 11:42:53 +0200

    Add password reset function in account settings

--- app/Livewire/Pages/Settings/Index.php
+++ app/Livewire/Pages/Settings/Index.php
@@ -5,10 +5,16 @@ namespace App\Livewire\Pages\Settings;
 use Livewire\Component;
 use Illuminate\Support\Facades\Auth;
 use App\Models\User;
+use App\Traits\HasFlashMessage;
 
 class Index extends Component
 {
+    use HasFlashMessage;
     public User $user;
+
+    public $old_password;
+    public $new_password;
+    public $password_confirm;
 
     public function mount()
     {
@@ -20,5 +26,44 @@ class Index extends Component
     public function render()
     {
         return view('livewire.pages.settings.index')->extends('layouts.app');
+    }
+
+    public function changePassword()
+    {
+
+        $this->validate([
+            'old_password' => 'required|min:8',
+            'new_password' => 'required|min:8|same:password_confirm',
+            'password_confirm' => 'required|min:8',
+        ]);
+
+        if (!Auth::attempt(['email' => $this->user->email, 'password' => $this->old_password])) {
+            $this->addError('old_password', 'Altes Passwort ist nicht korrekt.');
+            $this->new_password = '';
+            $this->password_confirm = '';
+            return;
+        }
+
+        if ($this->old_password === $this->new_password) {
+            $this->addError('new_password', 'Das neue Passwort darf nicht mit dem alten Passwort identisch sein.');
+            $this->new_password = '';
+            $this->password_confirm = '';
+            return;
+        }
+
+        $this->user->password = bcrypt($this->new_password);
+
+        $this->user->save();
+
+        $this->old_password = '';
+        $this->new_password = '';
+        $this->password_confirm = '';
+
+        // Log out the user
+        Auth::logout();
+
+        $this->flash('success', 'Dein Passwort wurde erfolgreich geändert! Bitte melde dich erneut an.', 6000);
+
+        return redirect()->route('login');
     }
 }
--- resources/views/livewire/pages/settings/index.blade.php
+++ resources/views/livewire/pages/settings/index.blade.php
@@ -1,15 +1,48 @@
 <div>
     <x-app.header :$user />
-    <div class="container">
-        <x-page-heading>{{ __('Account') }}</x-page-heading>
-    </div>
 
-    <div class="container">
-        <div class="mt-10 text-center">
-            <img
-                class="mx-auto"
-                src="{{ Vite::asset('resources/assets/images/work-in-progress.gif') }}"
-            />
-        </div>
+    <div class="container max-w-2xl text-light">
+        <x-page-heading>{{ __('Passwort ändern') }}</x-page-heading>
+
+        <form wire:submit.prevent="changePassword" novalidate>
+            <div class="mt-6">
+                <x-form.input
+                    wire:model.lazy="old_password"
+                    type="password"
+                    name="old_password"
+                    required
+                    class="bg-dark text-light"
+                    label="{{ __('Altes Passwort') }}"
+                />
+            </div>
+
+            <div class="mt-6">
+                <x-form.input
+                    wire:model.lazy="new_password"
+                    type="password"
+                    required
+                    name="new_password"
+                    class="bg-dark text-light"
+                    label="{{ __('Neues Passwort') }}"
+                />
+            </div>
+
+            <div class="mt-6">
+                <x-form.input
+                    wire:model.lazy="password_confirm"
+                    type="password"
+                    name="password_confirm"
+                    required
+                    class="bg-dark text-light"
+                    label="{{ __('Neues Passwort bestätigen') }}"
+                />
+            </div>
+
+            <div class="mt-12">
+                <x-button class="bg-primary text-light">
+                    <span>{{ __('Speichern') }}</span>
+                </x-button>
+            </div>
+        </form>
     </div>
 </div>
